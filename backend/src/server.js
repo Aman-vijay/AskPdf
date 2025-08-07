64a39605ed102a2a8c9a94eb834102c75e9a3e2c
Aman-vijay <shivamvj04@gmail.com>
2025-08-07 21:22:15 +0530

    Update server.js for pdf viewing

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,14 +13,13 @@ import { errorHandler } from './middleware/errorHandler.js';
 import { createUploadsDir } from './utils/fileUtils.js';
 const app = express();
 const PORT = process.env.PORT || 5000;
-const frontendUrl = process.env.FRONTEND_URL || 'http://localhost:5173';
+const frontendUrl = process.env.NODE_ENV === 'production' 
+  ? process.env.FRONTEND_URL_PROD:process.env.FRONTEND_URL;
 
 // Security middleware
 app.use(helmet());
 app.use(cors({
-  origin: process.env.NODE_ENV === 'production' 
-    ? process.env.FRONTEND_URL_PROD
-    : [frontendUrl],
+  origin: frontendUrl,
   credentials: true
 }));
 
@@ -37,10 +36,14 @@ app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
 
-app.use((req, res, next) => {
-  res.setHeader("Content-Security-Policy", `frame-ancestors 'self' ${frontendUrl}`);
-  next();
-});
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self'"],
+      frameAncestors: ["'self'", frontendUrl],
+    },
+  })
+);
 
 
 
